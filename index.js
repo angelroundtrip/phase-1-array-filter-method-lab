2a931ad62a4d54498bdf517432e545a2046f8886
Angel Rosario <arosario.us@gmail.com>
2022-01-17 18:35:25 -0500

    commit tree 2fab8e40c7563e690385c74652c6feb3a6bf9bcb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// Called functions
+
+function findMatching(){
+    ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+        return 'Bobby'
+    
+}
+
+function fuzzyMatch(){
+    
+}
+
+function matchName(){
+    
+}
